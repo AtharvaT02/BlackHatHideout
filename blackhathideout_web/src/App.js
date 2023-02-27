dd0703fe46310d9cd0398f0252f58a6924bc2b20
LoneWalker <atharvathorkar2002@gmail.com>
2023-02-27 20:59:34 +0530

    Added Signup and Signin Pages

--- blackhathideout_web/src/App.js
+++ blackhathideout_web/src/App.js
@@ -8,14 +8,122 @@ import "./assets/scss/blk-design-system-react.scss";
 import "./assets/demo/demo.css";
 import 'font-awesome/css/font-awesome.min.css';
 
-function App() {
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+
+
+import { useState, useEffect, useMemo } from "react";
+
+// react-router components
+import { Switch, Redirect, useLocation } from "react-router-dom";
+
+// @mui material components
+import Icon from "@mui/material/Icon";
+
+// Vision UI Dashboard React components
+import VuiBox from "components/VuiBox";
+
+// Vision UI Dashboard React example components
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
+
+// Vision UI Dashboard React themes
+import themeRTL from "assets/theme/theme-rtl";
+
+// RTL plugins
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+// Vision UI Dashboard React routes
+import routes from "routes";
+
+// Vision UI Dashboard React contexts
+import { useVisionUIController, setMiniSidenav, setOpenConfigurator } from "context";
+
+
+// Vision UI Dashboard React themes
+import theme from "assets/theme";
+
+export default function App() {
+  const [controller, dispatch] = useVisionUIController();
+  const { miniSidenav, direction, layout, openConfigurator, sidenavColor } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
+
+  // Cache for the rtl
+  useMemo(() => {
+    const cacheRtl = createCache({
+      key: "rtl",
+      stylisPlugins: [rtlPlugin],
+    });
+
+    setRtlCache(cacheRtl);
+  }, []);
+
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  // Change the openConfigurator state
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+
+  // Setting the dir attribute for the body element
+  useEffect(() => {
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} component={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
+  
   return (
     <>
-      <Router>
-        <LandingPage />
-      </Router>
-    </>
+      {layout === "dashboard" && (
+        <>
+         
+        </>
+      )}
+      {layout === "vr" && <Configurator />}
+      <Switch>
+        {getRoutes(routes)}
+        <Redirect from="*" to="/Home" />
+      </Switch>
+      </>
+      
   );
 }
 
-export default App;
+
